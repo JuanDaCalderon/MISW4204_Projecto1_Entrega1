ada54e90f5b33f37e67acd20cdf31e294eab8df4
IngSneyderAmado <s.amadov@uniandes.edu.co>
2023-11-29 19:51:59 -0500

    se agrega el convertir archivo

--- push.py
+++ push.py
@@ -1,8 +1,23 @@
 from flask import Flask, request, abort
 from google.cloud import pubsub_v1
 import tareas 
+from dotenv import load_dotenv
+load_dotenv('.env')
+from os import environ
+from modelos import db
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'+environ.get('PGSQL_USER')+':'+environ.get('PGSQL_PASSWORD')+'@'+environ.get('PGSQL_HOST')+'/'+ environ.get('PGSQL_DB')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
 
 # Configura las credenciales de Google Cloud
 # Debes configurar GOOGLE_APPLICATION_CREDENTIALS en tu entorno o proporcionar el archivo de credenciales.
@@ -37,14 +52,16 @@ def receive_push():
         # Puedes implementar la lógica de verificación de firma según tus necesidades de seguridad.
 
         # Procesa el mensaje recibido
-        message = request.get_json()
+        message = request.get_data()
         print(f"Mensaje recibido: {message}")
-        datos = str(message.data).split(',')
+        datos = str(message).split(',')
         datos[0] = datos[0].replace("b'","")
         datos[2] = datos[2].replace("'","")
         print(f"Received {datos[0], datos[1], int(datos[2])}")
 
         # Realiza cualquier lógica de procesamiento necesario aquí
+        
+        tareas.convertirArchivo(datos[0], datos[1], int(datos[2]),app_context)
 
         return 'procesado ...', 204  # 204 significa "No Content" para indicar que la solicitud fue procesada correctamente
 
@@ -53,4 +70,6 @@ def receive_push():
         abort(500, 'Internal Server Error')
 
 if __name__ == '__main__':
+    
+    
     app.run(host='0.0.0.0', port=8080, debug=True)
